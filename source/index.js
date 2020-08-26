8a0207d26f39c3813c2ee61293bbe34069630a7a
Alexander Rice <webdev@ricealexander.com>
2020-08-26 15:10:12 -0500

    :sparkles: Hook into Grove Navigation
    Every tenth of a second, check if user has navigated. If so, run all functions in an onNavigation array. This allows us to create custom functions which run on every page

--- source/index.js
+++ source/index.js
@@ -3,11 +3,27 @@ import { default as loadScript               } from './loadScript'
 import { default as loadStylesheet           } from './loadStylesheet'
 import { default as setIsolatedInterval      } from './setIsolatedInterval'
 
+// Configure STLPR object with helper functions
 const STLPR = {
   loadScript,
   loadStylesheet,
   addIsolatedEventListener,
   setIsolatedInterval,
 }
+
+// Hook into Grove Navigation
+STLPR.onNavigation = []
+STLPR.currentPage = window.location.pathname
+
+setInterval(() => {
+  const currentPage = window.location.pathname
+  const hasChangedPages = (currentPage !== STLPR.currentPage)
+
+  // If navigation occurred, run all functions in STLPR.onNavigation
+  if (hasChangedPages) {
+    STLPR.currentPage = currentPage
+    for (const callback of STLPR.onNavigation) callback()
+  }
+}, 100)
 
 export default STLPR
