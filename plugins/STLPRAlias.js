d0cedcc375a3596c3a47c19588599e2337de64ce
Alexander Rice <webdev@ricealexander.com>
2020-08-26 17:33:04 -0500

    :sparkles: Create STLPR Alias plugin
    STLPRAlias creates the global STLPR for compatability purposes

---
+++ plugins/STLPRAlias.js
@@ -0,0 +1,3 @@
+export default () => {
+  window.STLPR = window.Glade
+}
--- plugins/index.js
+++ plugins/index.js
@@ -1,6 +1,7 @@
 import { default as identifyCurrentPage } from './identifyCurrentPage'
 import { default as fixPodcastBylines   } from './fixPodcastBylines'
 import { default as makeAudioButton     } from './makeAudioButton'
+import { default as STLPRAlias          } from './STLPRAlias'
 
 // Plugin
 // fn:      plugin function
@@ -11,6 +12,7 @@ const plugins = [
   { fn: identifyCurrentPage, enabled: true },
   { fn: fixPodcastBylines,   enabled: true },
   { fn: makeAudioButton,     enabled: true },
+  { fn: STLPRAlias,          enabled: true },
 ]
 
 export default plugins
