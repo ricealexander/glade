3a7b9fe938135ec49aad901326fee4ddb81374cd
Alexander Rice <webdev@ricealexander.com>
2020-08-26 14:49:03 -0500

    :sparkles: Create loadScript and loadStylesheet
    :warning: There are a couple issues with this process so far.
    1. Parcel is increasing the bundle size by a factor of 10
    2. The code is being transpiled to ES5, but features such as Array methods and modern DOM methods still require polyfills.
    I may need to switch to a different bundling tool

--- source/loadScript.js
+++ source/loadScript.js
@@ -1,5 +1,20 @@
+// loadScript (source)
+// load a given script if not already present on the page
+
 function loadScript (source) {
-  return source
+  // Find all external scripts in the page
+  const scripts = [...document.querySelectorAll('script')]
+  const externalScripts = scripts.filter(script => script.src)
+
+  // Exit silently if script is already present
+  if (externalScripts.includes(source)) return
+
+  // Create a new script and inject it into the DOM
+  const newScript = document.createElement('script')
+  newScript.src = source
+
+  const lastScript = scripts[scripts.length - 1]
+  lastScript.after(newScript)
 }
 
 export default loadScript
--- source/loadStylesheet.js
+++ source/loadStylesheet.js
@@ -1,5 +1,18 @@
 function loadStylesheet (href) {
-  return href
+  // Find all stylesheets in the page
+  var links = [...document.querySelectorAll('link')]
+  var stylesheets = links.filter(link => link.rel === 'stylesheet')
+
+  // Exit silently if stylesheet is already present
+  if (stylesheets.includes(href)) return false
+
+  // Create a new stylesheet and inject it into the DOM
+  var element = document.createElement('link')
+  element.rel = 'stylesheet'
+  element.href = href
+
+  var lastLink = links[links.length - 1]
+  lastLink.after(element)
 }
 
 export default loadStylesheet
