717dd70165bc8c4bb64c49da12ef5f21fcaf2fca
Alexander Rice <webdev@ricealexander.com>
2020-12-12 15:51:46 -0600

    :sparkles: Allow loadScript to pass attributes

--- source/loadScript.js
+++ source/loadScript.js
@@ -1,7 +1,8 @@
+/* eslint-disable unicorn/prefer-modern-dom-apis */
 // loadScript (source)
 // load a given script if not already present on the page
 
-function loadScript (source) {
+function loadScript (source, attributes = {}) {
   // Find all external scripts in the page
   const scripts = [...document.querySelectorAll('script')]
   const externalScripts = scripts.filter(script => script.src)
@@ -13,8 +14,11 @@ function loadScript (source) {
   const newScript = document.createElement('script')
   newScript.src = source
 
+  if (attributes.async) newScript.async = attributes.async
+  if (attributes.defer) newScript.defer = attributes.defer
+
   const lastScript = scripts[scripts.length - 1]
-  lastScript.after(newScript)
+  lastScript.insertAdjacentElement('afterend', newScript)
 }
 
 export default loadScript
