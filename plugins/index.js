7b73bcdebd3e68e39e2d835afbba64ef581a2a81
Alexander Rice <webdev@ricealexander.com>
2023-01-12 13:51:40 -0600

    :fire: Deprecate fixPodcastBylines and longformBylineWorkaround plugins
    NPR implemented a Podcast byline fix in late 2022. The longform content type has also been deprecated in Grove

--- plugins/index.js
+++ plugins/index.js
@@ -1,13 +1,11 @@
 import { default as addThemeByTag       } from './addThemeByTag'
 import { default as addUMSLAttribution  } from './addUMSLAttribution'
+import { default as animateLogos        } from './animateLogos'
 import { default as identifyCurrentPage } from './identifyCurrentPage'
 import { default as IEBannerMessage     } from './IEBannerMessage'
-import { default as fixPodcastBylines   } from './fixPodcastBylines'
 import { default as formatTranscripts   } from './formatTranscripts'
 import { default as googleCustomSearch  } from './googleCustomSearch'
-import { default as longformBylineWorkaround  } from './longformBylineWorkaround'
 import { default as makePlaceholder     } from './makePlaceholder'
-import { default as animateLogos        } from './animateLogos'
 
 // Plugin
 // fn:      plugin function
@@ -15,20 +13,19 @@ import { default as animateLogos        } from './animateLogos'
 // args [optional]: arguments to pass into plugin
 
 const plugins = [
-  { fn: addThemeByTag,       enabled: true,
-    args: [],
-  },
   { fn: addUMSLAttribution,  enabled: true },
   { fn: animateLogos,        enabled: true },
   { fn: identifyCurrentPage, enabled: true },
+  { fn: formatTranscripts,   enabled: true },
+  { fn: googleCustomSearch,  enabled: true },
+  { fn: makePlaceholder,     enabled: true },
+
+  { fn: addThemeByTag,       enabled: true,
+    args: [],
+  },
   { fn: IEBannerMessage,     enabled: false,
     args: ['Not all features may be supported in Internet Explorer'],
   },
-  { fn: fixPodcastBylines,   enabled: true },
-  { fn: formatTranscripts,   enabled: true },
-  { fn: googleCustomSearch,  enabled: true },
-  { fn: longformBylineWorkaround,  enabled: true },
-  { fn: makePlaceholder,     enabled: true },
 ]
 
 export default plugins
