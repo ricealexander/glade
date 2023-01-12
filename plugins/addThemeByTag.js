c7de15ae229504fa728638badbcb263afcdcb798
Alexander Rice <webdev@ricealexander.com>
2023-01-12 15:45:33 -0600

    :recycle: Refactor addThemeByTag so that tags and themes can be passed into it as arguments

--- plugins/addThemeByTag.js
+++ plugins/addThemeByTag.js
@@ -1,20 +1,7 @@
 import onNavigate from '../lib/onNavigate'
 
 
-const themeCSS = `
-.stlpr-logo .logo-bar {
-  stroke-width: 0.5px;
-  stroke: currentColor;
-}
-
-.stlpr-logo .logo-bar-1 { color: #d62021; fill: #d62021 !important; }
-.stlpr-logo .logo-bar-2 { color: #faa21b; fill: #faa21b !important; }
-.stlpr-logo .logo-bar-3 { color: #006b71; fill: #006b71 !important; }
-.stlpr-logo .logo-bar-4 { color: #234093; fill: #234093 !important; }
-`
-
-
-function applyTheme () {
+function applyTheme (tags, theme) {
   if (!brightspotDataLayer) {
     throw new ReferenceError('Could not read Brightspot Data Layer.')
   }
@@ -23,15 +10,15 @@ function applyTheme () {
     throw new ReferenceError (`Could not read Tags from Data Layer, "${brightspotDataLayer.keywords}".`)
   }
 
-  const tags = brightspotDataLayer.keywords.trim().split(',')
+  const storyTags = brightspotDataLayer.keywords.trim().split(',')
 
-  if (tags.includes('LGBT') || tags.includes('LGBTQ')) {
-    Glade.insertCSS(themeCSS)
+  if (tags.any(tag => storyTags.includes(tag))) {
+    Glade.insertCSS(theme)
   }
 }
 
 
-export default () => {
+export default (...parameters) => {
   applyTheme()
-  onNavigate(() => applyTheme())
+  onNavigate(() => applyTheme(...parameters))
 }
--- plugins/index.js
+++ plugins/index.js
@@ -8,10 +8,24 @@ import { default as googleCustomSearch  } from './googleCustomSearch'
 import { default as makePlaceholder     } from './makePlaceholder'
 
 
+const LGBTTheme = `
+.stlpr-logo .logo-bar {
+  stroke-width: 0.5px;
+  stroke: currentColor;
+}
+
+.stlpr-logo .logo-bar-1 { color: #d62021; fill: #d62021 !important; }
+.stlpr-logo .logo-bar-2 { color: #faa21b; fill: #faa21b !important; }
+.stlpr-logo .logo-bar-3 { color: #006b71; fill: #006b71 !important; }
+.stlpr-logo .logo-bar-4 { color: #234093; fill: #234093 !important; }
+`
+
+
 // Plugin
-// fn:      plugin function
+// fn:      plugin function.
 // enabled: whether or not plugin should run
 // args [optional]: arguments to pass into plugin
+// A plugin can be used multiple times with different arguments
 
 const plugins = [
   { fn: addUMSLAttribution,  enabled: true },
@@ -22,7 +36,7 @@ const plugins = [
   { fn: makePlaceholder,     enabled: true },
 
   { fn: addThemeByTag,       enabled: true,
-    args: [],
+    args: [['LGBT', 'LGBTQ'], LGBTTheme],
   },
 
   { fn: IEBannerMessage,     enabled: false,
