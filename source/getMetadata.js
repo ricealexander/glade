a3462532200b49f5de82a30d17a467ccb6562c75
Alexander Rice <mail@ricealexander.com>
2024-05-14 14:55:29 -0500

    :fire: Remove fallback values in getMetaData
    With the shift to the brightspot-dataLayer meta tag, we no longer justify copying each target property one value at a time and setting fallback values. It is also worse to maintain for a few reasons:
    
    1. The only defaults that fundamentally changed the JSON were STLPR-specific values. The core of Glade should be station agnostic and only the Plugins should have station-specific content.
    
    2. Matching the object is more consistent. If brightspot-dataLayer meta tag and window.brightspotDataLayer both show undefined for a value, Glade.getMetadata should show undefined as well, not null.

--- source/getMetadata.js
+++ source/getMetadata.js
@@ -10,17 +10,9 @@ function getMetadata () {
 
   const dataLayer = JSON.parse(dataLayerElement.content)
 
-  dataLayer.authors        = splitString(dataLayer.author   || '')
-  dataLayer.categories     = splitString(dataLayer.category || '')
-  dataLayer.tags           = splitString(dataLayer.keywords || '')
-
-  dataLayer.hasInlineAudio = !!(dataLayer.inlineAudio || 0)
-  dataLayer.publishedDate  = dataLayer.publishedDate ? new Date(dataLayer.publishedDate) : ''
-  dataLayer.storyTitle     = dataLayer.storyTitle || document.querySelector('h1')?.textContent || document.title
-
-  dataLayer.siteName       = dataLayer.siteName   || 'STLPR'
-  dataLayer.station        = dataLayer.station    || 'St. Louis Public Radio'
-  dataLayer.timezone       = dataLayer.timezone   || 'US/Central'
+  dataLayer.authors    = splitString(dataLayer.author   || '')
+  dataLayer.categories = splitString(dataLayer.category || '')
+  dataLayer.tags       = splitString(dataLayer.keywords || '')
 
   return dataLayer
 }
