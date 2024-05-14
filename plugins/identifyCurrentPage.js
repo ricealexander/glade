3f7978375f9214bc043f06622e5df7fc94507bc9
Alexander Rice <mail@ricealexander.com>
2024-05-14 16:03:44 -0500

    :recycle: Prefer document.documentElement over document.body
    document.body may not exist at certain times while the page loads, but document.documentElement always will

--- plugins/identifyCurrentPage.js
+++ plugins/identifyCurrentPage.js
@@ -2,11 +2,8 @@ import onNavigate from '../lib/onNavigate'
 
 
 function identifyCurrentPage () {
-  if (!document.body) {
-    throw new ReferenceError('Could not locate document.body')
-  }
-
-  document.body.dataset.page = window.location.pathname
+  const html = document.documentElement
+  html.dataset.page = window.location.pathname
 }
 
 
