3ea87022c78e6d26c06672b5836d91d9d65f55d3
Alexander Rice <mail@ricealexander.com>
2024-05-14 17:00:58 -0500

    :recycle: Refactor persistence behavior in addIsolatedEventListener to use onNavigate

--- source/addIsolatedEventListener.js
+++ source/addIsolatedEventListener.js
@@ -1,21 +1,20 @@
+/* eslint-disable brace-style */
+
+import onNavigate from '../lib/onNavigate'
+
+
 // addIsolatedEventListener(element, type, callback)
 // a wrapper for addEventListener which self-destructs on page navigation
 
-function addIsolatedEventListener (element, type, callback) {
-  const currentPage = () => window.location.pathname
-  const startingPage = currentPage()
+function addIsolatedEventListener (element, type, callback, options) {
+  // Create the event listener
+  element.addEventListener(type, callback, options)
 
-  function isolatedCallback (event) {
-    var hasChangedPages = (startingPage !== currentPage())
-
-    if (hasChangedPages) {
-      element.removeEventListener(type, isolatedCallback)
-      return
-    }
-    callback(event)
-  }
-
-  element.addEventListener(type, isolatedCallback)
+  // Clear the listener when the user navigates away
+  onNavigate(
+    () => { element.removeEventListener(type, callback, options) },
+    { once: true }
+  )
 }
 
 
