7d97b78e8798847b859f0327fc1a68c49114942d
Alexander Rice <mail@ricealexander.com>
2024-05-14 16:08:36 -0500

    :sparkles: Identify if a page has 1 or 2 columns
    The right edge of articles and pages are aligned differently if they have 1 or 2 columns

--- plugins/identifyCurrentPage.js
+++ plugins/identifyCurrentPage.js
@@ -4,6 +4,10 @@ import onNavigate from '../lib/onNavigate'
 function identifyCurrentPage () {
   const html = document.documentElement
   html.dataset.page = window.location.pathname
+
+  let sidebar = document.querySelector('aside[class$="-aside-content"], div[class$="-aside-content"], div[class^=TwoColumnContainer]')
+  if (sidebar) html.dataset.columns = 2
+  else         html.dataset.columns = 1
 }
 
 
