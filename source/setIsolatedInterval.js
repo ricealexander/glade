974529b2604438a5d9add5c81947563073a5a282
Alexander Rice <mail@ricealexander.com>
2024-05-14 17:04:02 -0500

    :recycle: Refactor persistence behavior in setIsolatedInterval to use onNavigate

--- source/setIsolatedInterval.js
+++ source/setIsolatedInterval.js
@@ -1,23 +1,22 @@
+/* eslint-disable brace-style */
+
+import onNavigate from '../lib/onNavigate'
+
+
 // setIsolatedInterval(callback, milliseconds)
 // a wrapper for setInterval which self-destructs on page navigation
 
 function setIsolatedInterval (callback, milliseconds) {
-  const currentPage = () => window.location.pathname
+  // Create the interval
+  const interval = setInterval(callback, milliseconds)
 
-  const startingPage = currentPage()
-  let interval
+  // Clear interval when the user navigates
+  onNavigate(
+    () => { clearInterval(interval) },
+    { once: true }
+  )
 
-  function isolatedCallback () {
-    var hasChangedPages = (startingPage !== currentPage())
-
-    if (hasChangedPages) {
-      clearInterval(interval)
-      return
-    }
-    callback()
-  }
-
-  interval = setInterval(isolatedCallback, milliseconds)
+  // Return a reference to the interval
   return interval
 }
 
